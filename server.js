51a2c8dcc6c3f8cc00fe9003a9ac398f2f374b9b
vishwanath-sharma7 <87034825+vishwanath-sharma7@users.noreply.github.com>
2023-05-11 18:22:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -69,7 +69,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://192.168.2.228:8082/api"); // Replace * with the domain and port of your application
+  res.header("Access-Control-Allow-Origin", "https://phases-api.onrender.com/api"); 
+  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
